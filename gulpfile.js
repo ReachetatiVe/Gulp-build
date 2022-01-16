bb45d9e07b2516cb36fb6e4a5f426b67697d3250
ReachetatiVe <64168998+ReachetatiVe@users.noreply.github.com>
2022-01-16 18:02:37 +0300

    tru way for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
+//Импорт общих плагинов
+import { plugins } from "./gulp/config/plugins.js";
 
 global.app = {
   path: path,
   gulp: gulp,
+  plugins: plugins,
 }
 
 //tasks import
