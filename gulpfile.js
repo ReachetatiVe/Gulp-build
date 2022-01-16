bbb003767155d7fbf5ab5da110092fd9ce245792
ReachetatiVe <64168998+ReachetatiVe@users.noreply.github.com>
2022-01-16 17:45:13 +0300

    html task added

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,18 @@ global.app = {
 //tasks import
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 //Наблюдаель за изменениями в файлах
 function watcher() {
-  gulp.watch(path.watch.files, copy)
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
 }
 
+const mainTasks = gulp.parallel(copy, html);
+
 //Построение сценариев выполнения задач
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 
 //default task
 gulp.task('default', dev);
