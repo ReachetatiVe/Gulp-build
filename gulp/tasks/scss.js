1b618685df5e51d39b0df0051f9ffe9486fb1096
ReachetatiVe <64168998+ReachetatiVe@users.noreply.github.com>
2022-01-26 20:49:52 +0300

    css addons

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -2,6 +2,11 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 
+import cleanCss from 'gulp-clean-css'; //Сжатие
+import webpcss from "gulp-webpcss"; //Вывод webp изображений
+import autoPrefixer from 'gulp-autoprefixer'; //Доб-е вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; //Группировка media запросов
+
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
@@ -19,6 +24,19 @@ export const scss = () => {
     .pipe(sass({
       outputStyle: 'expanded'
     }))
+    .pipe(groupCssMediaQueries())
+    .pipe(webpcss({
+      webpClass: ".webp",
+      noWebpClass: ".no-webp"
+    }))
+    .pipe(autoPrefixer({
+      grid: true,
+      overrideBrowserslist: ["last 5 versions"],
+      cascade: true
+    }))
+    //Если нужен несжатый файл
+    // .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(cleanCss())
     .pipe(rename({
       extname: ".min.css"
     }))
