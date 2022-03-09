c022614cba579838d122859362848a5a7f40088c
Alex M <bertkesha45@gmail.com>
2022-03-09 22:00:43 +0300

    added creation zip

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontStyle } from "./gulp/tasks/fonts.js";
 import { svgSprive } from "./gulp/tasks/svgSprite.js";
+import {zip} from "./gulp/tasks/zip.js";
 
 //Наблюдаель за изменениями в файлах
 function watcher() {
@@ -40,9 +41,12 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 //Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
 
+//Экспорт сценариев
 export {dev}
 export {build}
+export {deployZIP}
 
 //default task
 gulp.task('default', dev);
